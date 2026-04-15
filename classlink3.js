a062c32a492e7cd7259a1386c25c6e258e9f52cb
Sleepygiraffe <awesomet149@gmail.com>
2026-04-15 01:37:25 -0400

    Update and rename classlink5.js to classlink3.js

--- classlink3.js
+++ classlink3.js
@@ -105,7 +105,7 @@ const apps = [
     {
         name: "Subway Surfers",
         url: "https://cdn.jsdelivr.net/gh/UncopylockDomainHere/testing/games/subwaysurfers.html",
-        icon: "https://static.wikia.nocookie.net/subwaysurf/images/c/c3/ThirtySixthAvatar.jpg/revision/latest/scale-to-width-down/250?cb=20180320175404"
+        icon: "https://static.wikia.nocookie.net/subwaysurf/images/c/c3/ThirtySixthAvatar.jpg"
     },
     {
         name: "Buckshot Roulette",

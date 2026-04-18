cecf2d4ed7317ab5a5b44f0a2643d86442d35bfd
Sleepygiraffe <awesomet149@gmail.com>
2026-04-17 21:11:29 -0400

    Update classlink3.js

--- classlink3.js
+++ classlink3.js
@@ -16,7 +16,7 @@ style.textContent = `
         padding: 20px;
         justify-content: center;
         box-sizing: border-box;
-        height: 500px;
+        height: 100vh;
         overflow-y: auto;
     }
 

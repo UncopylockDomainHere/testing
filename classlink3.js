31cead6a0e6fa8a1af25fae423a64fe3aba20a4d
Sleepygiraffe <awesomet149@gmail.com>
2026-05-07 23:33:11 -0400

    Update classlink3.js

--- classlink3.js
+++ classlink3.js
@@ -15,7 +15,7 @@ style.textContent = `
         padding: 20px;
         justify-content: center;
         box-sizing: border-box;
-        min-height: 100vh;
+        max-height: 100vh;
         overflow-y: auto;
         justify-items: center;
         align-content: start;

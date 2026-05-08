372fcba5f0826e2c713ade2fa0d744873f362812
Sleepygiraffe <awesomet149@gmail.com>
2026-05-07 20:27:51 -0400

    Update classlink3.js

--- classlink3.js
+++ classlink3.js
@@ -239,11 +239,6 @@ const apps = [
     url: 'https://cdn.jsdelivr.net/gh/UncopylockDomainHere/testing/games/doom-2.html',
     icon: 'https://cdn.jsdelivr.net/gh/freebuisness/covers@main/602.png',
   },
-  {
-    name: 'Doom 2',
-    url: 'https://cdn.jsdelivr.net/gh/UncopylockDomainHere/testing/games/doom-2.html',
-    icon: 'https://cdn.jsdelivr.net/gh/freebuisness/covers@main/602.png',
-  },
 ];
 
 // Create buttons
